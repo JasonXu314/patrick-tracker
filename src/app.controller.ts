b0a11753c067c6477ba90b585e4f16c0dbda200a
Jason Xu <Xinwen31415@gmail.com>
2023-05-31 07:15:48 -0500

    added different punishment weights

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Header, HttpCode, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Header, HttpCode, ParseEnumPipe, Post } from '@nestjs/common';
+import { AppService, Severity } from './app.service';
 
 @Controller()
 export class AppController {
@@ -7,22 +7,39 @@ export class AppController {
 
 	@Get('/')
 	public getPage(): string {
+		const hasSession = this.service.hasSession();
+
 		return `
 		<html data-theme="dark">
 			<head>
 				<title>Patrick Tracker</title>
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+				<link rel="stylesheet" href="/styles.css">
 			</head>
 			<body>
 				<main class="container">
 					<h1>Current Punishment: ${this.service.getIndex().toFixed(4)} min.</h1>
-					<div class="grid">
-						<form action="/punish" method="POST">
-							<button type="submit">Punish</button>
-						</form>
-						<form action="/reset" method="POST">
-							<button type="submit">Reset</button>
-						</form>
+					<div>
+						<div class="grid">
+							<form>
+								${
+									hasSession
+										? `
+								<div class="grid">
+									<button name="severity" type="submit" value="annoyance" formaction="/punish" formmethod="POST">Annoyance</button>
+									<button name="severity" type="submit" value="egregious" formaction="/punish" formmethod="POST">Egregious</button>
+								</div>
+								<div class="grid">
+									<button class="reset" type="submit" formaction="/reset" formmethod="POST">Reset</button>
+									<button class="reset" type="submit" formaction="/end" formmethod="POST">End Session</button>
+								</div>
+								`
+										: `
+								<button class="success" type="submit" formaction="/begin" formmethod="POST">Begin Session</button>
+								`
+								}
+							</form>
+						</div>
 					</div>
 				</main>
 			</body>
@@ -30,18 +47,60 @@ export class AppController {
 		`;
 	}
 
+	@Get('/styles.css')
+	public styles(): string {
+		return `
+			:is(button,input[type=submit],input[type=button],[role=button]).success {
+				--background-color: #73cb87;
+				--border-color: #73cb87;
+				--color: black;
+			}
+
+			:is(button,input[type=submit],input[type=button],[role=button]).reset {
+				--background-color: #f44846;
+				--border-color: #f44846;
+				--color: black;
+			}
+		`;
+	}
+
 	@Post('/punish')
 	@HttpCode(303)
 	@Header('Location', '/')
-	public punish(): void {
-		this.service.punish();
+	public punish(@Body('severity', new ParseEnumPipe(Severity)) severity: Severity): void {
+		if (severity === Severity.ANNOYANCE) {
+			this.service.annoyance();
+		} else {
+			this.service.egregious();
+		}
 	}
 
-	@Post('/reset')
+	@Post('/begin')
 	@HttpCode(303)
 	@Header('Location', '/')
-	public reset(): void {
-		this.service.reset();
+	public begin(): void {
+		this.service.beginSession();
+	}
+
+	@Post('/end')
+	public end(): string {
+		const finalIdx = this.service.getIndex();
+		this.service.endSession();
+
+		return `
+			<html data-theme="dark">
+				<head>
+					<title>Patrick Tracker - Results</title>
+					<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+				</head>
+				<body>
+					<main class="container">
+						<h1>Final Punishment: ${finalIdx} min.</h1>
+						<a href="/" role="button">Back</a>
+					</main>
+				</body>
+			</html>
+		`;
 	}
 }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+
+export enum Severity {
+	EGREGIOUS = 'egregious',
+	ANNOYANCE = 'annoyance'
+}
 
 @Injectable()
 export class AppService {
 	private _index = 0;
-	private _lastTime = Date.now();
+	private _lastTime = -1;
 
-	public punish(): void {
+	public beginSession(): void {
+		if (this._lastTime === -1) {
+			this._lastTime = Date.now();
+		} else {
+			throw new BadRequestException('Session has already been started');
+		}
+	}
+
+	public annoyance(): void {
+		if (this._lastTime === -1) {
+			throw new BadRequestException('No session in progress');
+		}
+
 		const now = Date.now();
-		this._index += 30 / ((now - this._lastTime) / 1000);
+		this._index += 120 / ((now - this._lastTime) / 1000);
+		this._lastTime = now;
+	}
+
+	public egregious(): void {
+		if (this._lastTime === -1) {
+			throw new BadRequestException('No session in progress');
+		}
+
+		const now = Date.now();
+		this._index += (120 / ((now - this._lastTime) / 1000)) ** 2;
 		this._lastTime = now;
 	}
 
@@ -15,9 +42,13 @@ export class AppService {
 		return this._index;
 	}
 
-	public reset(): void {
+	public endSession(): void {
 		this._index = 0;
-		this._lastTime = Date.now();
+		this._lastTime = -1;
+	}
+
+	public hasSession(): boolean {
+		return this._lastTime !== -1;
 	}
 }
 
